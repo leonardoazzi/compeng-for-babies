ecebdd469ea7757601b924b53e4dfbd180bb16eb
Beatriz Soviero <79771418+biasoviero@users.noreply.github.com>
2024-08-16 22:30:28 -0300

    .

--- src/textrendering.cpp
+++ src/textrendering.cpp
@@ -368,7 +368,6 @@ void TextRendering_ShowMouseCoords(GLFWwindow* window)
     float pad = TextRendering_LineHeight(window);
 
     char buffer[80];
-    snprintf(buffer, 80, "Position coordinates = Z(%.2f)*Y(%.2f)*X(%.2f)\n", g_AngleZ, g_AngleY, g_AngleX);
     snprintf(buffer, 80, "Cursor = X(%.2f), Y(%.2f)\n", g_LastCursorPosX, g_LastCursorPosY);
 
     TextRendering_PrintString(window, buffer, -1.0f+pad/10, -1.0f+2*pad/10, 1.0f);
